3252868b2f30b71d0490763634fa7add8de1b5b5
onionfriends2004 <alupenkov@inbox.ru>
2024-04-07 02:14:42 +0300

    feat hw3: add urls

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request):
+    bug_list_url = reverse('quality_control:bug_list')
+    feature_list_url = reverse('quality_control:feature_list')
+    return HttpResponse(f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br/><a href='{feature_list_url}'>Запросы на улучшение</a>")
+
+def bug_list(request):
+    return HttpResponse(f'<h1>Список отчетов об ошибках</h1>')
+
+def feature_list(request):
+    return HttpResponse(f'<h1>Список запросов на улучшение</h1>')
+
+def bug_detail(request, bug_id):
+    return HttpResponse(f'Детали бага {bug_id}')
+
+def feature_detail(request, feature_id):
+    return HttpResponse(f'Детали улучшения {feature_id}')
